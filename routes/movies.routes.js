472b74ec438796c54ca7fdf4ac0b228c07bcb141
Josep <josepcortes6@gmail.com>
2022-06-19 20:13:25 +0200

    i7 solved

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,13 +1,27 @@
 const express = require('express');
-const { redirect } = require('express/lib/response');
-const async = require('hbs/lib/async');
-const app = require('../app');
 const router = express.Router();
 
 const Movie = require('../models/movie.model');
 const Celebrity = require('../models/Celebrity.model');
 
-//ruta para mostrar
+app.get('/', (req,res,next) => {
+    res.render('movies/movies')
+})
+
+app.get('/', async (req,res,next) => {
+   try {
+       const movies = await Movie.find()
+       res.render('movies/movies', {movies})
+   } catch (e) {
+       console.log(e)
+   }
+})
+
+//rutas para mostrar y crear
+app.get('/create', (req,res,next) => {
+    res.render('movies/new-movie');
+})
+
 app.get('/create',  async (req,res,next) =>{
      try{
          const celebrities = await Celebrity.find({celebrities})
@@ -17,15 +31,18 @@ app.get('/create',  async (req,res,next) =>{
          console.log(e)
      }
 })
-// ruta para crear
+
 app.post('/create', async(req,res,next) => {
     const {title, genre, spot, cast} = req.body;
     try{
         const newMovie = {title, genre, spot, cast}
-        await Movies.create(newMovie)
-        res.redirect('movies/new-movies')
+        await Movie.create(newMovie)
+        res.redirect('movies/create')
     }
     catch(e){
         console.log(e)
+        res.render('movies/new-movie')
     }
 })
+
+module.exports= router
